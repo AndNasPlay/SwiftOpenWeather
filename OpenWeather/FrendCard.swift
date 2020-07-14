bbaeb8311c1abf306cea73d4759212089a1b26b8
AndNasPlay <57287272+AndNasPlay@users.noreply.github.com>
2020-07-14 11:38:58 +0300

    Lesson 3

---
+++ OpenWeather/FrendCard.swift
@@ -0,0 +1,14 @@
+//
+//  FrendCard.swift
+//  OpenWeather
+//
+//  Created by Андрей Щекатунов on 14.07.2020.
+//  Copyright © 2020 Andrey Shchekatunov. All rights reserved.
+//
+
+import UIKit
+
+struct FrendCardStart {
+    let age: Int
+    let city: String
+}
